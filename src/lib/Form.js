f8abfbf3ec248a6d717639001a225f5ef20fa95d
Lisa Hammarstrand <lisa@Lisas-Air.lan>
2020-06-24 11:03:44 +0200

    Minor adjustments

--- src/lib/Form.js
+++ src/lib/Form.js
@@ -5,11 +5,15 @@ position: absolute;
 display: flex;
 flex-direction: column;
 top: 50px;
-width: 80%;
+width: 90%;
 height: auto;
 padding: 40px 30px 30px 30px;
 background: transparent;
 border: 2px solid white;
 border-radius: 6px;
 z-index: 1;
+
+@media (min-width: 650px) {
+  max-width: 620px;
+}
 `
